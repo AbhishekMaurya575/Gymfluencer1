8a6e010e459e69c22c4843e4a9ccc254c27f2c92
AbhishekMaurya575 <abhishek.maurya257185@gmail.com>
2024-12-19 00:48:15 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Header from "./components/Header/Header";
+
+import Section2 from "./Sections/Section2";
+import Section3 from "./Sections/About";
+import Section1 from "./Sections/Home";
+import Section4 from "./Sections/Section4";
+import Section5 from "./Sections/Section5";
+import Section6 from "./Sections/Section6";
+
+
+function App() {
+  return (
+    <>
+    
+      <Header />
+     <Section1/>
+      <Section2 />
+      <Section3 />
+      <Section4/>
+    <Section5/>
+    <Section6/>
+     
+     
+    </>
+  );
+}
+
+export default App;
