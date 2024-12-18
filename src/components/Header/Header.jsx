7ce82820c571e0d00c9f511eb021522d49377622
AbhishekMaurya575 <abhishek.maurya257185@gmail.com>
2024-12-19 01:19:18 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,13 @@ const Header = () => {
     <nav className='w-full  bg-none justify-center items-center flex h-[20vh] sm:[15vh] md:[20vh] absolute z-10  '>
       <div className='bg-black/90 text-white p-4 px-2 pl-4 flex  justify-around items-center h-[80px] w-[95vw] sm:w-[74vw] rounded-full border-white/40' style={{fontFamily: "Orbitron, 'Orbitron Placeholder', sans-serif" , fontSize:"18px",borderWidth:"1px"}}>
       <div className='w-full sm:w-[142px]'>
-        <img src='public\asset 0.svg' className='w-[142px] h-[36px] text-xl' />
+        <img src='public\assets\asset 0.svg' className='w-[142px] h-[36px] text-xl' />
       </div>
       <div className='w-[50vw] '>
         <ul className='lg:flex hidden justify-around' style={{color:"#96979c"}} >
           <li>Home</li>
           <li>About</li>
-          <li className='flex'>Our Services<img src='public\asset 1.svg' className='w-[30px]'/></li>
+          <li className='flex'>Our Services<img src='public\assets\asset 1.svg' className='w-[30px]'/></li>
           <li>Benefits</li> 
           <li>Blogs</li>
           <li>Contact</li>
